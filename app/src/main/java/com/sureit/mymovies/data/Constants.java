4c7f830c6cdc12a81417ab4a4b4ec06a800b0b82
sureshkumarm8 <sureshkumarm8@gmail.com>
2018-07-15 13:49:48 +0530

    Trailer Module added.

---
+++ app/src/main/java/com/sureit/mymovies/data/Constants.java
@@ -0,0 +1,14 @@
+package com.sureit.mymovies.data;
+
+import com.sureit.mymovies.BuildConfig;
+
+public class Constants {
+    public static final String BASE_URL_MOVIE = "https://api.themoviedb.org/3/discover/movie?api_key=";
+    public static final String POPULAR_MOVIES_URL = "https://api.themoviedb.org/3/movie/popular?api_key=";
+    public static final String TOP_RATED_MOVIES_URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=";
+    public static final String TRAILERS_MOVIES_URL = "https://api.themoviedb.org/3/movie/";
+
+    public static final String API_KEY = BuildConfig.MovieSecAPIKEY;
+
+    public static final String PARCEL_KEY="MovieParcel";
+}
